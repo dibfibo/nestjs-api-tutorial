3ef55c26c330fd9f04bbe2935a8f9ffd7e7c071e
dibfibo <dibfibo@gmail.com>
2024-03-10 19:26:43 +0100

    auth+
    inject prisma service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  constructor() {}
+  constructor(private Prisma: PrismaService) {}
 
   signup() {
     return { msg: 'I am signed up' };
