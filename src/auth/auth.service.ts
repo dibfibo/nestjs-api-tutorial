ab567bfa20cc57c1ca7c3a84c5616d9db16cae33
dibfibo <dibfibo@gmail.com>
2024-03-10 20:37:15 +0100

    auth+
    signup error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -11,18 +12,27 @@ export class AuthService {
     // generate password hash using argon 2
     const hash = await argon.hash(dto.password);
 
-    // save new user in db
-    const user = await this.Prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    //return saved user
+    try {
+      // save new user in db
+      const user = await this.Prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      //return saved user
 
-    delete user.hash
+      delete user.hash;
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error
+      }
+    }
   }
 
   signin() {
