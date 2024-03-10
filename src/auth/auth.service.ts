c433d1db6b513451aca6b15cb5477f7819bda814
dibfibo <dibfibo@gmail.com>
2024-03-10 18:42:11 +0100

    initial modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  constructor() {}
+
+  signup() {
+    return { msg: 'I am signed up' };
+  }
+
+  signin() {
+    return { msg: 'I am signed in' };
+  }
+}
