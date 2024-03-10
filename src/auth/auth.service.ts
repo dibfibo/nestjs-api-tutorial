f0e1620fb74802c1fd46d179c16a7eb1c84522b2
dibfibo <dibfibo@gmail.com>
2024-03-10 20:18:23 +0100

    auth
    create and return user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
   constructor(private Prisma: PrismaService) {}
 
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // generate password hash using argon 2
+    const hash = await argon.hash(dto.password);    
+    
     // save new user in db
+    const user = await this.Prisma.user.create({
+        data: {
+            email: dto.email,
+            hash
+        }
+    })
     //return saved user
 
-    return { msg: 'I am signed up' };
+    return user;
   }
 
   signin() {
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
-  email: String;
+  email: string;
 
   @IsString()
   @IsNotEmpty()
