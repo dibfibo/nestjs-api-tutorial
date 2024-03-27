1ee7308a6007af8e061df255ac5d4432b6da1c8a
dibfibo <dibfibo@gmail.com>
2024-03-27 23:12:29 +0100

    +

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,10 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '@prisma/client';
 
 export const GetUser = createParamDecorator(
-  (data: Exclude<keyof User | undefined, 'hash'>, ctx: ExecutionContext) => {
+  (data: unknown, ctx: ExecutionContext) => {
     const req: Express.Request = ctx.switchToHttp().getRequest();
 
-    return data ? req.user : req.user[data];
+    return req.user;
   },
 );
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { Request } from 'express';
-import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @UseGuards(JwtGuard)
