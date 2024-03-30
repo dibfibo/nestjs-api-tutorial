2399935ebfd68304b2a2a6a5cf8e45da28f1c0d5
dibfibo <dibfibo@gmail.com>
2024-03-30 21:51:50 +0100

    nest - 0.0.4 - dev --publication

--- bash/publication-tag.sh
+++ bash/publication-tag.sh
@@ -8,14 +8,14 @@ if [ -z "$1" ]; then
 fi
 
 # define environment name
-environment_name="$1"
+environment="$1"
 
 
 # read project name
-project_tag=$(grep -o '"tag": *"[^"]*"' package.json | sed 's/"tag": "\(.*\)"/\1/')
+tag=$(grep -o '"tag": *"[^"]*"' package.json | sed 's/"tag": "\(.*\)"/\1/')
 
 # check project name
-if [ -z "$project_tag" ]; then
+if [ -z "$tag" ]; then
     echo "Error: Cannot find tag in package.json file."
     exit 1
 fi
@@ -30,7 +30,7 @@ if [ -z "$version" ]; then
 fi
 
 # define commit message
-message="$project_tag - $version - $environment_name --publication"
+message="$tag - $version - $environment --publication"
 
 # stage changes
 git add .
@@ -42,10 +42,10 @@ git commit -m "$message"
 timestamp=$(date +"%Y%m%d%H%M%S")
 
 # define tag name
-tag_name="$project_tag-$environment_name-$timestamp"
+tag="$tag-$environment-$timestamp"
 
 # create tag
-git tag "$tag_name"
+git tag "$tag"
 
 # push head with tags
 git push origin HEAD --tags
