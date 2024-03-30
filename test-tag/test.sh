24f5d30fda55cf4d972bbee0be3b06da46346845
dibfibo <dibfibo@gmail.com>
2024-03-30 15:59:01 +0100

    project_name - pubblicazione in staging

--- test-tag/test.sh
+++ test-tag/test.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Controlla se è stato fornito il nome dell'ambiente come argomento
+if [ -z "$1" ]; then
+    echo "Errore: specificare il nome dell'ambiente come argomento dello script."
+    exit 1
+fi
+
 # Recupera il nome del progetto
 project_name="project_name"
 
 # Recupera il nome dell'ambiente
-environment_name="environment_name" # Imposta manualmente o recupera da una variabile
+environment_name="$1" # Imposta manualmente o recupera da una variabile
+
+# Crea il messaggio
+message="$project_name - pubblicazione in $environment_name"
+
+# stage changes
+git add .
+
+# commit changes
+git commit -m "$message"
 
 # Genera timestamp
 timestamp=$(date +"%Y%m%d%H%M%S")
@@ -12,11 +27,8 @@ timestamp=$(date +"%Y%m%d%H%M%S")
 # Crea il nome del tag
 tag="$project_name-$environment_name-$timestamp"
 
-# Crea il messaggio
-message="$project_name - pubblicazione in $environment_name"
-
 # Crea il tag
-git tag "$tag" -m "$message"
+git tag "$tag"
 
 # Pusha il tag
 git push origin "$tag"
